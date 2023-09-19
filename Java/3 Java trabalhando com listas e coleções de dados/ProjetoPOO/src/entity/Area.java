138568a9af4fc8bdc4eb74d1db0eea6ca3be4d09
José Paiva <josecarlospaivasantos2001@gmail.com>
2023-09-19 15:03:58 -0300

    .

--- Java/3 Java trabalhando com listas e coleções de dados/ProjetoPOO/src/entity/Area.java
+++
@@ -1,7 +0,0 @@
-package entity;
-
-public enum Area {
-    ClínicaGeral, Pediátra, GinecologiaeObstetrícia, Dermatologia,
-    Oftalmologia, Otorrinolaringologia, Neurologia, Cardiologia,
-    Radiologia;
-}
--- Java/3 Java trabalhando com listas e coleções de dados/ProjetoPOO/src/entity/HospitalArea.java
+++
@@ -1,5 +0,0 @@
-package entity;
-
-public enum HospitalArea {
-    Ambulatório, Emergência, UTI, BlocoCirúrgico;
-}
