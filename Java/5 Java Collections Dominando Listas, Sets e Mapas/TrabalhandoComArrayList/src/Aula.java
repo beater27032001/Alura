f7fe5ba348165fb016696e8d121e31c1e7f24f7c
José Paiva <josecarlospaivasantos2001@gmail.com>
2023-09-27 03:12:28 -0300

    .

--- Java/5 Java Collections Dominando Listas, Sets e Mapas/TrabalhandoComArrayList/src/Aula.java
+++ Java/5 Java Collections Dominando Listas, Sets e Mapas/TrabalhandoComArrayList/src/Aula.java
@@ -1,28 +1,30 @@
-public class Aula implements Comparable<Aula>{
+package br.com.alura;
 
-    private String titulo;
-    private int tempo;
+public class Aula implements Comparable<Aula> {
 
-    public Aula(String titulo, int tempo) {
-        this.titulo = titulo;
-        this.tempo = tempo;
-    }
+	private String titulo;
+	private int tempo;
 
-    public String getTitulo() {
-        return titulo;
-    }
+	public Aula(String titulo, int tempo) {
+		this.titulo = titulo;
+		this.tempo = tempo;
+	}
 
-    public int getTempo() {
-        return tempo;
-    }
+	public String getTitulo() {
+		return titulo;
+	}
 
-    @Override
-    public String toString() {
-        return "[Aula: " + this.titulo + ", " + this.tempo + " minutos]";
-    }
+	public int getTempo() {
+		return tempo;
+	}
 
-    @Override
-    public int compareTo(Aula outraA) {
-        return this.titulo.compareTo(outraA.titulo);
-    }
+	@Override
+	public String toString() {
+		return "[Aula: " + this.titulo + ", " + this.tempo + " minutos]";
+	}
+
+	@Override
+	public int compareTo(Aula outraAula) {
+		return this.titulo.compareTo(outraAula.getTitulo());
+	}
 }
